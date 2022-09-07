29e2f31a10c06184624667682199e8b91dd70a30
PT400C Jomcraft <pt400c@gmail.com>
2022-09-07 15:51:32 +0200

    Add MoreDecorations endpoint

---
+++ product/moredecorations.php
@@ -0,0 +1,17 @@
+<?php
+header("Content-Type:application/json");
+$productName = "moredecorations";
+$memory = new Memcached();
+$memory->addServer("127.0.0.1", 11211);
+
+include __DIR__ . '/utils.php';
+
+if (isset($_GET['versions']) && $_GET['versions'] == "") {
+    versions($productName , $memory);
+} else if (isset($_GET['latest']) && $_GET['latest'] != "") {
+    latest($_GET['latest'], $productName , $memory);
+} else if (isset($_GET['endpoint']) && $_GET['endpoint'] != "") {
+    endpoint($_GET['endpoint'], $productName , $memory);
+} else {
+    http_response_code(400);
+}
